9be62f705ed8d546e5e304c6f435bef1e02dd813
George <georuiz817@gmail.com>
2019-09-22 11:33:58 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  
+  let taskDescription = document.querySelector('#new-task-description')
+
+  let ulTasks = document.querySelector("#tasks")
+
+
+  form.addEventListener("submit", addTask)
+
+  function addTask(event){
+    event.preventDefault()
+    let newLi = document.createElement('li')
+    newLi.textContent = taskDescription.value
+    let taskPriority = document.querySelector("#priority").value
+    newLi.style.color = taskPriority  
+    let deleteButton = document.createElement('button')
+    deleteButton.innerText = "X"
+    newLi.appendChild(deleteButton)
+    deleteButton.addEventListener('click', deleteTask)
+    ulTasks.appendChild(newLi)
+  }
+
+  function deleteTask(event){
+    event.currentTarget.parentElement.remove()
+  }
+
+  
 });

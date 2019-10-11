a6788c8a73a8579e541aabbc006158572aa12eaf
latinawhocodes <alyssaroque01@gmail.com>
2019-10-11 03:51:54 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('form').addEventListener('submit', submitForm);
+  loadTasks()
 });
+
+function submitForm(event) {
+  event.preventDefault();
+  let description = document.getElementById("new-task-description").value
+  let ul = document.getElementById("tasks")
+  ul.innerHTML += `<li>${description}<li>`
+}
+
+function loadTasks() {
+  let ul = document.getElementById("tasks")
+  loadTasks.forEach(function(task) {
+    ul.innerHTML += `<li>${task.description}<li>`
+  });
+}
